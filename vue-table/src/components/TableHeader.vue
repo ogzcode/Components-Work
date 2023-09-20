21ddcd7273b07dc7339d455fc11ebfa48d386ba1
ogzCode <oguzguc44@gmail.com>
2023-09-20 15:58:08 +0300

    column sort function moved to store

--- vue-table/src/components/TableHeader.vue
+++ vue-table/src/components/TableHeader.vue
@@ -1,13 +1,25 @@
 <template>
     <thead>
         <tr>
-            <th v-for="column in dataTableStore.headers" 
-                :key="column.key" 
-                :style="{ width: column.width }"
-                @click="handleSort(column.key, column.sortEnabled)"
-                >
+            <th v-for="column in dataTableStore.headers" :key="column.key" :style="{ width: column.width }"
+                @click="handleSort(column.key, column.sortEnabled)">
                 {{ column.columnName }}
-                <span v-html="sortArrow" v-if="columnLabelAndOrder.columnName === column.key"></span>
+                <span v-if="columnLabelAndOrder.columnName === column.key">
+                    <span v-if="columnLabelAndOrder.order === 'asc'">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-caret-up-fill" viewBox="0 0 16 16">
+                            <path
+                                d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
+                        </svg>
+                    </span>
+                    <span v-else-if="columnLabelAndOrder.order === 'desc'">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                        </svg>
+                    </span>
+                </span>
             </th>
         </tr>
     </thead>
@@ -25,8 +37,6 @@ const columnLabelAndOrder = ref({
     order: ''
 })
 
-const emit = defineEmits(["on-sort"])
-
 const handleSort = (columnName, sortEnabled) => {
     if (!sortEnabled) return;
 
@@ -38,24 +48,8 @@ const handleSort = (columnName, sortEnabled) => {
         columnLabelAndOrder.value.order = 'asc';
     }
 
-    emit('on-sort', columnLabelAndOrder.value);
+    dataTableStore.sort(columnLabelAndOrder.value);
 }
-
-const sortArrow = computed(() => {
-    if (columnLabelAndOrder.value.order === 'asc') {
-        return `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-up-fill" viewBox="0 0 16 16">
-  <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
-</svg>`;
-    }
-    else if (columnLabelAndOrder.value.order === 'desc') {
-        return `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
-  <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-</svg>`;
-    }
-    else {
-        return '';
-    }
-})
 </script>
 
 <style scoped>
