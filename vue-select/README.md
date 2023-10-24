425f55bcdc0b7f9a7f05cc2a6305f9af53e235eb
ogzCode <oguzguc44@gmail.com>
2023-10-24 12:08:49 +0300

    Select component created with Vue.js and Tailwind CSS

--- vue-select/README.md
+++ vue-select/README.md
@@ -1,5 +1,6 @@
-# vue-select bileşeni
-Vye.js ve Tailwind.css ile geliştirilen Pure select bileşeni.
+# ./
+
+This template should help get you started developing with Vue 3 in Vite.
 
 ## Recommended IDE Setup
 
--- vue-select/src/App.vue
+++ vue-select/src/App.vue
@@ -1,78 +1,51 @@
 <template>
   <div class="min-h-screen flex justify-center items-center">
     <div class="w-[480px]">
-      <Select v-model="select" :items="items" />
+      <Select v-model="selected" :items="items" />
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import Select from './components/Select.vue';
+import Select from './components/Select.vue'
 
-const select = ref(null)
-
+const selected = ref(null)
 const items = ref([
   {
     value: '1',
-    label: 'Item 1ewrljghnljerhgjklehrljghljerhgjlherjkgjkekgjhqşjrhewhgjlhwrgherhşl',
+    label: 'Item 1'
   },
   {
     value: '2',
-    label: 'Item 2',
+    label: 'Item 2'
   },
   {
     value: '3',
-    label: 'Item 3',
+    label: 'Item 3'
   },
   {
     value: '4',
-    label: 'Item 4',
+    label: 'Item 4'
   },
   {
     value: '5',
-    label: 'Item 5',
+    label: 'Item 5'
   },
   {
     value: '6',
-    label: 'Item 6',
+    label: 'Item 6'
   },
   {
-    value: '5',
-    label: 'Item 5',
+    value: '7',
+    label: 'Item 7'
   },
   {
-    value: '6',
-    label: 'Item 6',
-  },
-  {
-    value: '5',
-    label: 'Item 5',
-  },
-  {
-    value: '6',
-    label: 'Item 6',
-  },
-  {
-    value: '5',
-    label: 'Item 5',
-  },
-  {
-    value: '6',
-    label: 'Item 6',
-  },
-  {
-    value: '5',
-    label: 'Item 5',
-  },
-  {
-    value: '6',
-    label: 'Item 6',
+    value: '8',
+    label: 'Item 8'
   }
 ])
-
 </script>
-
 <style scoped>
 
 </style>
