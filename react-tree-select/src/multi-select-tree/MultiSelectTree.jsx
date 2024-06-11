f3639d558f6027d9f328af9b80d38258fca3a9a6
ogzCode <oguzguc44@gmail.com>
2024-06-11 08:57:44 +0300

    feat: Add multi-select tree component and related files

---
+++ react-tree-select/src/multi-select-tree/MultiSelectTree.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import { ChevronDown } from "lucide-react"
+import { ScrollArea } from "@/components/ui/scroll-area"
+
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+
+import {TreeItem} from "./TreeItem"
+
+export default function MultipleSelectTree({ options, selectedList, onChangeSelectedList }) {
+    const [open, setOpen] = useState(false)
+
+    return (
+        <div>
+            <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                    <Button variant="outline" onClick={() => setOpen(true)} className="font-normal flex justify-between items-center min-w-[20rem]">
+                        <span>
+                            {
+                                selectedList?.length === 0 ? "Select items" :
+                                    `${selectedList?.length} items selected`
+                            }
+                        </span>
+                        <ChevronDown className="h-4 w-4 opacity-50" />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="p-2 w-[20rem]">
+                    <ScrollArea className="min-h-[10rem]">
+                        {options.map((option, index) => (
+                            <TreeItem
+                                key={index}
+                                label={option.label}
+                                value={option.value}
+                                child={option.children}
+                                selectedList={selectedList}
+                                onChecked={onChangeSelectedList}
+                            />
+                        ))}
+                    </ScrollArea>
+                </PopoverContent>
+            </Popover>
+        </div>
+    )
+}
